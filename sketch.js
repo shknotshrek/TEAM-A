a8c6508ef783c081ec5ef94a1a43232accd47ba6
shknotshrek <suehwa@snu.ac.kr>
2025-05-25 10:59:35 +0900

    이미지 배율 수정

--- sketch.js
+++ sketch.js
@@ -1,29 +1,29 @@
 let img;
 
 function preload() {
-  img = loadImage("your-image-file.png"); // 이미지 파일명에 맞게 수정
+  img = loadImage("visual assets/street.png");
 }
 
 function setup() {
-  createCanvas(1512, 982); // 맥북 14인치 기준 화면
+  createCanvas(1512, 982); // 혹은 windowWidth, windowHeight로 바꿔도 돼
   imageMode(CENTER);
+  noLoop();
 }
 
 function draw() {
   background(0);
 
-  // 이미지 비율 유지하며 화면에 맞게 크기 조절
   let canvasRatio = width / height;
   let imgRatio = img.width / img.height;
 
   let newW, newH;
 
-  if (imgRatio > canvasRatio) {
-    // 이미지가 더 가로로 길면 → 가로를 기준으로 맞추기
+  if (imgRatio < canvasRatio) {
+    // 캔버스가 더 가로로 긴 경우 → 가로에 맞춰 확대 (세로 일부 잘림)
     newW = width;
     newH = width / imgRatio;
   } else {
-    // 이미지가 더 세로로 길면 → 세로를 기준으로 맞추기
+    // 캔버스가 더 세로로 긴 경우 → 세로에 맞춰 확대 (가로 일부 잘림)
     newH = height;
     newW = height * imgRatio;
   }
