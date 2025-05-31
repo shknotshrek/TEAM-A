01f2c3dbe48de8e2f80ef6ca8e554097926fee09
shknotshrek <suehwa@snu.ac.kr>
2025-05-31 15:25:53 +0900

    음원 재생 시작 위치 수정

--- sketch.js
+++ sketch.js
@@ -1010,7 +1010,7 @@ function draw() {
 
   // 제목 인트로 음원
   if (
-    ["screen1", "screen2", "screen2-1", "screen2-2", "screen2-3", "screen2-4", "screen3", "screen3-1", "screen4", "screen5", "screen6", "screen7-intro"].includes(currentKey)
+    ["screen2", "screen2-1", "screen2-2", "screen2-3", "screen2-4", "screen3", "screen3-1", "screen4", "screen5", "screen6", "screen7-intro"].includes(currentKey)
     && !introMusicStarted
   ) {
     introMusic.loop(); // 반복 재생
@@ -1019,7 +1019,7 @@ function draw() {
 
   // 👉 다른 화면으로 넘어가면 멈추고 플래그 리셋
   if (
-    !["screen1", "screen2", "screen2-1", "screen2-2", "screen2-3", "screen2-4", "screen3", "screen3-1", "screen4", "screen5", "screen6", "screen7-intro"].includes(currentKey)
+    !["screen2", "screen2-1", "screen2-2", "screen2-3", "screen2-4", "screen3", "screen3-1", "screen4", "screen5", "screen6", "screen7-intro"].includes(currentKey)
     && introMusicStarted
   ) {
     introMusic.stop();
